cc1ca04989a025a91c9e42d6b3fc1dc173d0387a
fededomm <fededomm@outlook.it>
2023-12-15 17:41:09 +0100

    better format main.go and database.go

--- database/database.go
+++ database/database.go
@@ -2,13 +2,29 @@ package database
 
 import (
 	"database/sql"
+	"fmt"
+	"strconv"
 
 	_ "github.com/lib/pq"
 	"github.com/rs/zerolog/log"
 )
 
-func Init(cn string) *sql.DB {
-	db, err := sql.Open("postgres", cn)
+func Init(config *DbInfo) *sql.DB {
+	intPort, err := strconv.Atoi(config.Port)
+	if err != nil {
+		log.Fatal().Err(err).Msgf("Error convert Port to int: %q", err)
+	}
+
+	connectionString := fmt.Sprintf("host=%s port=%d user=%s password=%s dbname=%s sslmode=%s",
+		config.Host,
+		intPort,
+		config.User,
+		config.Password,
+		config.Dbname,
+		config.Sslmode,
+	)
+	
+	db, err := sql.Open("postgres", connectionString)
 	if err != nil {
 		log.Err(err).Msgf("Error opening database: %q", err)
 	}
--- main.go
+++ main.go
@@ -3,9 +3,6 @@ package main
 import (
 	"apocalypse/database"
 	"apocalypse/rest"
-	"database/sql"
-	"fmt"
-	"strconv"
 
 	"github.com/rs/zerolog/log"
 )
@@ -16,37 +13,11 @@ func main() {
 		log.Fatal().Err(err).Msgf("Error reading config: %q", err)
 	}
 
-	intPort, err := strconv.Atoi(conf.DataBaseConfig.Port)
-	if err != nil {
-		log.Fatal().Err(err).Msgf("Error convert Port to int: %q", err)
-	}
-
-	connectionString := fmt.Sprintf("host=%s port=%d user=%s password=%s dbname=%s sslmode=%s",
-		conf.DataBaseConfig.Host,
-		intPort,
-		conf.DataBaseConfig.User,
-		conf.DataBaseConfig.Password,
-		conf.DataBaseConfig.Dbname,
-		conf.DataBaseConfig.Sslmode,
-	)
-
-	db := database.Init(connectionString)
+	db := database.Init(&conf.DataBaseConfig)
 	if err != nil {
 		log.Fatal().Err(err).Msgf("Error handling database connection: %q", err)
 	}
 	log.Info().Msg("Database connection established")
-	log.Info().Msgf("Database connection established, connection string: %s", connectionString)
 	rest.Router(db)
 }
 
-func handleDBConnection(db *sql.DB) (*sql.DB, error) {
-	defer func() {
-		if err := db.Close(); err != nil {
-			log.Error().Err(err).Msg("Error closing database connection")
-			return
-		} else {
-			log.Info().Msg("Database connection closed")
-		}
-	}()
-	return db, nil
-}
